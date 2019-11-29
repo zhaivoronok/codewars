ad192bb02ee97ef8738f704f8877228adeb675c8
zhaivoronok <zhaivoronokev@gmail.com>
2019-11-30 01:02:53 +0200

    refactored

--- 004-multiples-of-3-or-5/index.js
+++ 004-multiples-of-3-or-5/index.js
@@ -1,14 +1,13 @@
 // https://www.codewars.com/kata/514b92a657cdc65150000006
 
 function solution(number) {
-  let numArray = [];
+  let sum = 0;
 
   for (let i = 1; i < number; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
-      console.log(i);
-      numArray.push(i);
+    if (!(i % 3) || !(i % 5)) {
+      sum += i;
     }
   }
 
-  return numArray.reduce((sum, e) => sum + e, 0);
+  return sum;
 }
