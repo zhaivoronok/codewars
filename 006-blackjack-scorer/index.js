de8d09c2c414be0b0907e164fbe66922d6441b46
zhaivoronok <zhaivoronokev@gmail.com>
2019-11-30 21:10:53 +0200

    new kata done

---
+++ 006-blackjack-scorer/index.js
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/534ffb35edb1241eda0015fe
+
+function scoreHand(cards) {
+  let values = 0;
+
+  for (let key in cards) {
+    switch (cards[key]) {
+      case 'J':
+      case 'Q':
+      case 'K':
+        values += 10;
+        break;
+      case 'A':
+        values += 11;
+        break;
+      default:
+        values += +cards[key];
+        break;
+    }
+  }
+
+  for (let key in cards) {
+    if (cards[key] === 'A' && values > 21) values -= 10;
+  }
+
+  return values;
+}
